3f7dc191348cc66423fd154abf065658e1cca9ef
Rajeeb Banstola <rajeebthegreat@gmail.com>
2016-04-13 17:38:11 +0545

    Fix the table layout
    Thanks to http://stackoverflow.com/questions/36595671/how-to-properly-format-json-data-in-table-using-angularjs/36595942#36595942

--- js/app.js
+++ js/app.js
@@ -1,19 +1,12 @@
 var app = angular.module("PowerCut", []);
 
 app.controller("ScheduleCtrl", function($scope, $http) {
-	$http.get('js/data.json').
-	success(function(data, status, headers, config) {
-		$scope.schedule = data[0];
-		$scope.singledayschedule = [];
-
-		angular.forEach($scope.schedule, function(value, key) {
- 			angular.forEach(value, function(value, key) {
- 				$scope.singledayschedule.push(value);
-			});
-		});
-	console.log($scope.singledayschedule);
-	}).
-	error(function(data, status, headers, config) {
-	// log error
-	});
+  $http.get('js/data.json').
+  success(function(data, status, headers, config) {
+    $scope.singledayschedule = data[0];
+    delete $scope.singledayschedule.updated;
+  }).
+  error(function(data, status, headers, config) {
+    // log error
+  });
 });
